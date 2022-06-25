4fe92c180b8a020c901c8e3025a06c6299b240ea
Yana Petrachenko <Yana.Petrachenko@emc.com>
2022-06-25 10:42:02 +0300

    Fixed indents

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,24 +10,24 @@ import string
 
 @pytest.fixture()
 def get_random_brewery():
-	""" Function to get random brewery.
-	
-	:param: None
-	:returns: brewery record
-	:rtype: str
-	"""
-	random_url = "{host}/{breweries}/{random}".format(host=cs.HOST, breweries=cs.BREWERIES_ENDPOINT, random=cs.RANDOM_ENDPOINT)
-	random_brewery = requests.get(random_url).json()[0]
-	
-	return random_brewery
+    """ Function to get random brewery.
+
+    :param: None
+    :returns: brewery record
+    :rtype: str
+    """
+    random_url = "{host}/{breweries}/{random}".format(host=cs.HOST, breweries=cs.BREWERIES_ENDPOINT, random=cs.RANDOM_ENDPOINT)
+    random_brewery = requests.get(random_url).json()[0]
+
+    return random_brewery
 	
 	
 @pytest.fixture()
 def generate_random_string():
-	""" Function to generate random string for negative testing.
+    """ Function to generate random string for negative testing.
 	
-	:param: None
-	:returns: random string
-	:rtype: str
-	"""
-	return "".join(random.choices(string.ascii_lowercase, k=random.randint(1, 50)))
+    :param: None
+    :returns: random string
+    :rtype: str
+    """
+    return "".join(random.choices(string.ascii_lowercase, k=random.randint(1, 50)))
